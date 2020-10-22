11f8a630c7a78b30346c056db169859dcf902271
Jan Slabon <jan.slabon@setasign.com>
2020-10-22 16:45:14 +0200

    Added "Image to PDF with pre-defined resolution" demo

---
+++ public/demos/1-Core/images/image-with-resolution/script.php
@@ -0,0 +1,36 @@
+<?php
+
+// load and register the autoload function
+require_once '../../../../../bootstrap.php';
+
+// prepare some files
+$files = glob($assetsDirectory . '/images/*/*.{png,jpg,jpeg,gif}', GLOB_BRACE);
+
+displayFiles($files, true, ['dpi' => [72, 96, 150, 300]]);
+
+// create a writer
+$writer = new SetaPDF_Core_Writer_Http('ImgInSpecificResolution.pdf', true);
+// create a document
+$document = new SetaPDF_Core_Document($writer);
+
+$img = SetaPDF_Core_Image::getByPath($_GET['f']);
+$xObject = $img->toXObject($document);
+$width = $xObject->getWidth();
+$height = $xObject->getHeight();
+
+// calculate the width by the given DPI value
+$dpi = isset($_GET['dpi']) ? abs($_GET['dpi']) : 72;
+$dpi = $dpi == 0 ? 72 : $dpi;
+
+$width = $width * 72 / $dpi;
+$height = $height * 72 / $dpi;
+
+$pages = $document->getCatalog()->getPages();
+$page = $pages->create(
+    [$width, $height],
+    SetaPDF_Core_PageFormats::ORIENTATION_AUTO
+);
+$canvas = $page->getCanvas();
+$xObject->draw($canvas, 0, 0, $width, $height);
+
+$document->save()->finish();
