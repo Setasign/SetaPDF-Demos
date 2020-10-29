4b5ac77690cd22ec54a05d273f5fbc00f77b49df
Jan Slabon <jan.slabon@setasign.com>
2020-10-29 16:06:57 +0100

    Added "Filter By Font Size" demo

---
+++ public/demos/2-Extractor/filter/by-font-size/script.php
@@ -0,0 +1,45 @@
+<?php
+
+// load and register the autoload function
+require_once __DIR__ . '/../../../../../bootstrap.php';
+
+$data = [
+    '24' => '24pt',
+    '18' => '18pt',
+    '12' => '12pt'
+];
+
+$fontSize = displaySelect('Filter by:', $data);
+
+// create a document instance
+$document = SetaPDF_Core_Document::loadByFilename($assetsDirectory . '/pdfs/Brand-Guide.pdf');
+
+// create an extractor instance
+$extractor = new SetaPDF_Extractor($document);
+
+// create the word strategy...
+$strategy = new SetaPDF_Extractor_Strategy_Word();
+// ...and pass it to the extractor
+$extractor->setStrategy($strategy);
+
+// creat an instance of the font size filter
+$filter = new SetaPDF_Extractor_Filter_FontSize((float)$fontSize);
+// ...pass it to the strategy
+$strategy->setFilter($filter);
+
+// get access to the document pages
+$pages = $document->getCatalog()->getPages();
+
+// iterate over the pages and extract the words:
+for ($pageNo = 1; $pageNo <= $pages->count(); $pageNo++) {
+
+    $words = $extractor->getResultByPageNumber($pageNo);
+    echo '<b>' . count($words) . ' word(s) found on Page #' . $pageNo
+        . ' with the font size ' . $fontSize . 'pt:</b><ul>';
+
+    foreach ($words as $word) {
+        echo '<li>' . htmlspecialchars($word->getString()) . '</li>';
+    }
+
+    echo '</ul>';
+}
