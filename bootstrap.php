d7dd0635dc487d1f2f97305d59e2a91bbebdf5f7
Jan Slabon <jan.slabon@setasign.com>
2020-12-21 16:23:29 +0100

    Update bootstrap.php
    Display errors only on localhost

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', $_SERVER['SERVER_NAME'] === 'localhost' ? 1 : 0);
 date_default_timezone_set('Europe/Berlin');
 
 if (is_file(__DIR__ . '/../library/SetaPDF/Autoload.php')) {
