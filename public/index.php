3f8baa978406e233789bdfbaccdca4e4f8587a16
Jan Slabon <jan.slabon@setasign.com>
2026-05-19 12:09:50 +0200

    Update index.php
    Invalidate style.css cache

--- public/index.php
+++ public/index.php
@@ -139,7 +139,7 @@ echo <<<HTML
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="stylesheet" type="text/css" href="./layout/normalize.css"/>
-    <link rel="stylesheet" type="text/css" href="./layout/style.css"/>
+    <link rel="stylesheet" type="text/css" href="./layout/style.css?2"/>
     <link rel="stylesheet" type="text/css" href="./js/codemirror-5.61.1/codemirror.css"/>
     <link rel="canonical" href="{$canonical}" />
     <script type="text/javascript" src="./js/jquery-3.6.0.min.js"></script>
